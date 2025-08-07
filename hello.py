167e292562c22ff2d582875220f3dfb3b3cfb0d5
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-07 22:37:00 +0600

    .

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+a = 2
+b = 330
+print("A") if a > b else print("B")
