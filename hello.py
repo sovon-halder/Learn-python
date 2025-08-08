ed694d00f13ab44f61a47d3fe2b4fc1c93a534c1
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-08 18:21:06 +0600

    ....

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-day = 4
-match day:
-  case 6:
-    print("Today is Saturday")
-  case 7:
-    print("Today is Sunday")
-  case _:
-    print("Looking forward to the Weekend")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
