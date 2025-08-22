b71a95f172fb9db82cda7fdb5e975d311ca25a6d
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-22 23:33:02 +0600

    json

--- hello.py
+++ hello.py
@@ -1,30 +1,10 @@
-class Car:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
+import json
 
-  def move(self):
-    print("Drive!")
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
 
-class Boat:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
+# parse x:
+y = json.loads(x)
 
-  def move(self):
-    print("Sail!")
-
-class Plane:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
-
-  def move(self):
-    print("Fly!")
-
-car1 = Car("Ford", "Mustang")       #Create a Car object
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
-plane1 = Plane("Boeing", "747")     #Create a Plane object
-
-for x in (car1, boat1, plane1):
-  x.move()
+# the result is a Python dictionary:
+print(y["age"])
