b7dbefe9375892e23adbc700e92cc9cc7bb4843d
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-23 23:32:15 +0600

    user input

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-import json
-
-# some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
-print(y["age"])
+print("Enter your name:")
+name = input()
+print(f"Hello {name}")
