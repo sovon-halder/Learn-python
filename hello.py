9d4e9b902aec18e20ecb5165b69526c592be020c
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-29 23:17:01 +0600

    ...

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-# Empty list
-x = []
+my_array = [7, 12, 9, 4, 11, 8]
+minVal = my_array[0]
 
-# List with initial values
-y = [1, 2, 3, 4, 5]
+for i in my_array:
+  if i < minVal:
+    minVal = i
 
-# List with mixed types
-z = [1, "hello", 3.14, True]
+print('Lowest value:', minVal)
