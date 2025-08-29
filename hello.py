b27c5299d16228d412f6bdc65b190bb56f15cfc4
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-29 23:15:58 +0600

    py-dsa

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-print("Enter your name:")
-name = input()
-print(f"Hello {name}")
+# Empty list
+x = []
+
+# List with initial values
+y = [1, 2, 3, 4, 5]
+
+# List with mixed types
+z = [1, "hello", 3.14, True]
