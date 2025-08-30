db4a3e77e0139c10c918bc4b06e15a5b7f9ec000
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-30 22:34:54 +0600

    strint

--- hello.py
+++ hello.py
@@ -1,25 +1,2 @@
-stack = []
-
-# Push
-stack.append('A')
-stack.append('B')
-stack.append('C')
-print("Stack: ", stack)
-
-# Peek
-topElement = stack[-1]
-print("Peek: ", topElement)
-
-# Pop
-poppedElement = stack.pop()
-print("Pop: ", poppedElement)
-
-# Stack after Pop
-print("Stack after Pop: ", stack)
-
-# isEmpty
-isEmpty = not bool(stack)
-print("isEmpty: ", isEmpty)
-
-# Size
-print("Size: ",len(stack))
+a="hello sovon"
+print(a)
