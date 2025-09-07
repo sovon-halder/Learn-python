6797ae8cc82dd2e99647f150366834070d1d8a84
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-07 19:39:48 +0600

    input

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-l1=[6,7,8,55,77,5,4]
-l1.reverse()
-
-print(l1)
-print(type(l1))
-
-l2=(2,4,5,6,7,8,44,77)
-print(type(l2))
+fruits=[]
+f1=input("enter fruit name")
+fruits.append(f1)
+print(fruits)
