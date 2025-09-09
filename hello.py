a365d8e08be6d2d66bc649c509a02d7a03e71760
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-09 22:20:29 +0600

    dic

--- hello.py
+++ hello.py
@@ -1,17 +1,6 @@
-marks=[]
-f1=int(input("enter number"))
-marks.append(f1)
-f2=int(input("enter number"))
-marks.append(f2)
-f3=int(input("enter number"))
-marks.append(f3)
-f4=int(input("enter number"))
-marks.append(f4)
-f5=int(input("enter number"))
-marks.append(f5)
-f6=int(input("enter number"))
-marks.append(f6)
-f7=int(input("enter number"))
-marks.append(f7)
-marks.sort()
-print (marks)
+markes={
+    "sovon":100,
+    "rohan":300,
+    "hary":34
+}
+print(markes)
