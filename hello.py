ea471661267c74b6d7dc50805e9c10c3ad0255d6
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-09 06:34:06 +0600

    sort

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
-import re
-
-txt = "The rain in Spain"
-x = re.findall("Portugal", txt)
-print(x)
+marks=[]
+f1=int(input("enter number"))
+marks.append(f1)
+f2=int(input("enter number"))
+marks.append(f2)
+f3=int(input("enter number"))
+marks.append(f3)
+f4=int(input("enter number"))
+marks.append(f4)
+f5=int(input("enter number"))
+marks.append(f5)
+f6=int(input("enter number"))
+marks.append(f6)
+f7=int(input("enter number"))
+marks.append(f7)
+marks.sort()
+print (marks)
