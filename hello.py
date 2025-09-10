ed58880c42f4432b40f3a44e1e00cc11d7d27568
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-10 20:18:33 +0600

    face detct

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-markes={
-    "sovon":100,
-    "rohan":300,
-    "hary":34
-}
-print(markes)
+import cv2
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+webcam=cv2.VideoCapture(0)
+while True:
+    _,img=webcam.read()
+    cv2.imshow("face detection",img)
+    key=cv2.waitKey(10)
+    if key==27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
