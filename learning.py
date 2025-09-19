71d8420f67c47309a82aaaf88e8def4d5c1b19eb
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-20 02:40:55 +0600

    even or odd

--- learning.py
+++ learning.py
@@ -1,9 +1,5 @@
-markes1=int(input("Enter your markes1"))
-markes2=int(input("Enter your markes2"))
-markes3=int(input("Enter your markes3"))
-total_percentage= (markes1+markes2+markes3)/300
-
-if (total_percentage>=40):
-    print("your percentage is over than 40%")
-else :
-    print("your percentage is under than 40%")
+num=int(input("Enter a number: "))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
