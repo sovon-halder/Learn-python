94ce501929a8039849cf4b6dde042fde85c5b714
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-21 09:01:22 +0600

    comment check

--- learning.py
+++ learning.py
@@ -1,14 +1,8 @@
-num1=int(input("Enter a number: "))
-num2=int(input("Enter a number: "))
-num3=int(input("Enter a number: "))
-num4=int(input("Enter a number: "))
-
-if num1>num2 and num1>num3 and num1>num4:
-    print("number 1 is greter than all numbers")
-elif num2>num3 and num2>num4 and num2>num1:
-    print("number 2 is greter than all numbers")
-elif num3>num4 and num3>num1 and num3>num2:
-    print("number 3 is greter than all numbers")
-elif num4>num1 and num4>num2 and num4>num3:
-    print("number 4 is greter than all numbers")
-
+s="money is power"
+s2="love is everything"
+s3="click here"
+massage=input("enter your comment: ")
+if((s in massage) or (s2 in massage) or (s3 in massage)):
+    print("this is spam")
+else:
+    print("this is valid,you can go")
