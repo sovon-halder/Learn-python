f12f81376d57f68396f6521bfec7bb5a27ae6ce8
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-23 19:22:26 +0600

    fact

--- learning.py
+++ learning.py
@@ -1,6 +1,6 @@
 
-n=int(input())
-for i in range(2, n):
-    if (n%i)==0:
-        print("nnumber is not prime")
-        break
+n=int(input("Enter the number: "))
+product=1
+for i in range(1,n+1):
+    product*=i
+    print(f"the factorial of {n} is {product}")
