7b9b89f7abb5689acf01a2485230f87bb4497f67
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-28 20:55:56 +0600

    function

--- learning.py
+++ learning.py
@@ -1,6 +1,9 @@
-n1=int(input("enter your number"))
-n2=int(input("enter your number"))
-n3=int(input("enter your number"))
+def avg():
+    n1 = int(input("enter your number"))
+    n2 = int(input("enter your number"))
+    n3 = int(input("enter your number"))
 
-ave=(n1+n2+n3)/3
-print(ave)
+    ave = (n1 + n2 + n3) / 3
+    print(ave)
+
+avg()
