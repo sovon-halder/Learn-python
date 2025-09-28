412a31584a7b6d4f62b710601d66df8ae191fb18
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-09-28 21:13:04 +0600

    argu

--- learning.py
+++ learning.py
@@ -1,9 +1,3 @@
-def avg():
-    n1 = int(input("enter your number"))
-    n2 = int(input("enter your number"))
-    n3 = int(input("enter your number"))
-
-    ave = (n1 + n2 + n3) / 3
-    print(ave)
-
-avg()
+def name(ln):
+    print("sovon"+ln)
+name(" halder")
