bef7723200266b6079cbac53b4b52dde7723cc8c
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-10-02 11:50:22 +0600

    compear number

--- learning.py
+++ learning.py
@@ -1,7 +1,17 @@
-def name(name,ending="thank you"):
-    print(f"good day ,{name}")
-    print(ending)
+def comp():
 
-name("sovon")
+        n1 = int(input("Enter 1st number: "))
+        n2 = int(input("Enter 2nd number: "))
+        n3 = int(input("Enter 3rd number: "))
 
-name("rohan")
+        if (n1 < n2 and n1 < n3):
+            print(f"{n1} is smallest number")
+        elif (n2 < n1 and n2 < n3):
+            print(f"{n2} is smallest number")
+        else:
+            print(f"{n3} is smallest number")
+
+
+
+
+comp()
