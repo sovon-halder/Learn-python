a6a5b27802e4006b50000403ca5f0156d9d95282
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-10-14 00:16:50 +0600

    games

--- learning.py
+++ learning.py
@@ -1,10 +1,21 @@
 
-def rem(l, word):
-    for item in l:
-         l.remove(word)
-         return l
+computer = -1
+you = input("enter your choice: ")
+youDict = {"s":1, "w":-1, "g":0}
+you = youDict[you]
 
 
-l=["sovon","rohit","subha","nekhil"]
-
-print(rem(l,"sovon"))
+if(computer == -1 and you == 1):
+    print("you win")
+elif(computer == -1 and you == 0):
+    print("you lose")
+if(computer == 1 and you == -1):
+    print("you lose")
+elif(computer == 1 and you == 0):
+    print("you win")
+if(computer == 0 and you == -1):
+    print("you lose")
+elif(computer == 0 and you == 1):
+    print("you win")
+else:
+    print("something went wrong!")
