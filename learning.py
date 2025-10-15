8ada12789f9ec7626bbc28f036a72278325712c7
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-10-15 21:52:17 +0600

    gassing games

--- learning.py
+++ learning.py
@@ -1,21 +1,51 @@
+# import random
+# computer = random.choice([-1,0,1])
+# you = input("enter your choice: ")
+# youDict = {"s":1, "w":-1, "g":0}
+# you = youDict[you]
+#
+# if(computer == you):
+#     print("its a draw")
+# else:
+#     if(computer == -1 and you == 1):
+#         print("you win")
+#     elif(computer == -1 and you == 0):
+#         print("you lose")
+#     if(computer == 1 and you == -1):
+#         print("you lose")
+#     elif(computer == 1 and you == 0):
+#         print("you win")
+#     if(computer == 0 and you == -1):
+#         print("you lose")
+#     elif(computer == 0 and you == 1):
+#         print("you win")
+#     else:
+#         print("something went wrong!")
+#
 
-computer = -1
-you = input("enter your choice: ")
-youDict = {"s":1, "w":-1, "g":0}
+
+
+import random
+
+computer = random.choice([-1, 0, 1])
+you = input("enter your choice (s for snake, w for water, g for gun): ")
+
+youDict = {"s": 1, "w": -1, "g": 0}
 you = youDict[you]
 
-
-if(computer == -1 and you == 1):
-    print("you win")
-elif(computer == -1 and you == 0):
-    print("you lose")
-if(computer == 1 and you == -1):
-    print("you lose")
-elif(computer == 1 and you == 0):
-    print("you win")
-if(computer == 0 and you == -1):
-    print("you lose")
-elif(computer == 0 and you == 1):
-    print("you win")
+if computer == you:
+    print("It's a draw")
+elif (computer == -1 and you == 1):
+    print("You win")
+elif (computer == -1 and you == 0):
+    print("You lose")
+elif (computer == 1 and you == -1):
+    print("You lose")
+elif (computer == 1 and you == 0):
+    print("You win")
+elif (computer == 0 and you == -1):
+    print("You lose")
+elif (computer == 0 and you == 1):
+    print("You win")
 else:
-    print("something went wrong!")
+    print("Something went wrong!")
